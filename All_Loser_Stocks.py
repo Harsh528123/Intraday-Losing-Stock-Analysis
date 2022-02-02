102d3244ccb6d45d17559b7214313c3ee67a5ebe
Harsh Kothari <76859508+Harsh528123@users.noreply.github.com>
2022-02-02 11:44:58 -0700

    Cleaned up code

--- All_Loser_Stocks.py
+++ All_Loser_Stocks.py
@@ -1,36 +1,35 @@
 import sqlite3
-import time
 import ssl
 from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
 from urllib.parse import urljoin
 from urllib.parse import urlparse
-from datetime import datetime, timedelta
-from requests import get
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-import re
-import bs4
-import itertools
 
-
+''' 
+This program is to get the required tickers so that it can be inputed into the alpha vantage api
+'''
 url = input('Enter link:')
 if len(url)==1:
     url="https://www.tradingview.com/markets/stocks-usa/market-movers-losers/"
+    
 html = urllib.request.urlopen(url,context=ctx).read()
+# ugly html
 soup = BeautifulSoup(html, "html.parser")
 rawlist=list()
 for small_data in soup.find_all("a",{"class":"tv-screener__symbol"}):
-    #finds all data with necessary filters 
+    # searches for tags with specific class attribute 
     slink=small_data['href']
     #filters out content from attribute
     rawlist.append(slink)
 tickerlist=rawlist[0::2]
-#makes sure names only comes once
+#makes sure names only comes once and grabs the ticker symbol
 officialticker=list()
 for ticker in tickerlist:
     tlist=ticker.split("-")
     newticker=tlist[1].replace("/","")
+    # cleans up for the official ticker symbol
     officialticker.append(newticker)
 #print(officialticker)
